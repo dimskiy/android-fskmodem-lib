fc377f0ef8524bfde2bf15466b496ea0614331e7
dimskiy <vadim.sinitskiy@gmail.com>
2022-09-10 13:48:49 +0300

    Add Jitpack aar publication

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -4,9 +4,35 @@ plugins {
 }
 
 android {
+    namespace = "cytec.android.fskmodem"
+
     defaultConfig {
         minSdk = 19
         targetSdk = 32
         compileSdk = 32
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    buildOutputs.all {
+        val variantOutputImpl = this as com.android.build.gradle.internal.api.BaseVariantOutputImpl
+        variantOutputImpl.outputFileName = "android-fskmodem-${variantOutputImpl.name}.aar"
+    }
+
+    afterEvaluate {
+        publishing {
+            publications {
+                create<MavenPublication>("release") {
+                    from(components.getByName("release"))
+
+                    groupId = "com.github.dimskiy"
+                    artifactId = "android-fskmodem"
+                    version = "1.0"
+                }
+            }
+        }
     }
 }
